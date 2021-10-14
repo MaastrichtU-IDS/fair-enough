1da6fcb9381651a7adb3432510fc8003db2ce92b
Vincent Emonet <vincent.emonet@gmail.com>
2021-10-14 18:38:35 +0200

    fix Any import

--- backend/app/graphql.py
+++ backend/app/graphql.py
@@ -1,6 +1,6 @@
 import strawberry
 from strawberry.asgi import GraphQL
-from typing import List, Optional, Union
+from typing import List, Optional, Union, Any
 from pydantic import BaseModel, Field
 import json
 
