129a7fa9101037a175ab655b4d3ab739fe33cb72
Vincent Emonet <vincent.emonet@gmail.com>
2021-10-15 11:30:18 +0200

    remove comments

--- server/server.ts
+++ server/server.ts
@@ -3,10 +3,7 @@ import compression from 'compression';
 import helmet from 'helmet';
 import path from "path";
 // import cors from 'cors';
-// import * as swaggerUi from 'swagger-ui-express';
 
-
-// Now we define the Express server:
 export const app = express();
 // For production (cf. https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/deployment)
 app.use(compression());
@@ -15,36 +12,6 @@ app.use(helmet({
   contentSecurityPolicy: false,
 }));
 // app.use(cors());
-
-// // Add RESTful API endpoint with Sofa at /api
-// const openApi = OpenAPI({
-//   schema,
-//   info: {
-//     title: 'Bio2KG Registry API',
-//     version: '3.0.0',
-//   },
-// });
-// app.use(
-//   useSofa({
-//       basePath: '/api',
-//       schema,
-//       onRoute(info) {
-//           openApi.addRoute(info, {
-//             basePath: '/api',
-//           });
-//         },
-//   })
-// );
-// Add OpenAPI docs at /apidocs
-// openApi.save('./swagger.yml');
-// app.use('/apidocs', swaggerUi.serve, swaggerUi.setup(openApi.get()));
-
-
-// We could use an express server
-// app.use('/mysub-application1', express.static(path.resolve(__dirname, `../public`)))
-// app.get('/mysub-application1/*', (req,res) => { //this is required to support any client side routing written in react.
-//  res.sendFile(path.join(__dirname, '../../public', 'index.html'))
-// })
 
 // Serve React app at /
 app.use(express.static(path.join(__dirname, ".", "public")));
