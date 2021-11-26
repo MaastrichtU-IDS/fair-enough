f3ffd1aadbd252547b4b1bf4f1163e2b3eed5f3d
Vincent Emonet <vincent.emonet@gmail.com>
2021-11-26 12:52:01 +0100

    remove google search for searchable assessement

--- backend/app/assessments/f4_searchable.py
+++ backend/app/assessments/f4_searchable.py
@@ -1,17 +1,16 @@
 from app.models import AssessmentModel, EvaluationModel
 import requests
-from googlesearch import search
+# from googlesearch import search
 
 class Assessment(AssessmentModel):
     fair_type = 'f'
     metric_id = '4'
     role = 'check'
     title = 'The resource is indexed in a searchable resource'
-    description = """Search for existing metadata about the resource URI in data repositories, such as DataCite, RE3data. 
-Search for the URI using the resource title in search engines (Google)"""
+    description = """Search for existing metadata about the resource URI in data repositories, such as DataCite, RE3data."""
     author = 'https://orcid.org/0000-0002-1501-1082'
     max_score = 1
-    max_bonus = 1
+    max_bonus = 0
 
     def evaluate(self, eval: EvaluationModel, g):
         datacite_endpoint = 'https://api.datacite.org/repositories'
@@ -31,7 +30,7 @@ Search for the URI using the resource title in search engines (Google)"""
                 print(datacite_json['data']['attributes'].keys())
                 # ['id', 'type', 'attributes', 'relationships']
                 if datacite_data:
-                    self.bonus('Retrieved metadata about ' + doi + ' from DataCite API')
+                    self.success('Retrieved metadata about ' + doi + ' from DataCite API')
                     eval.data['datacite'] = {}
                     print('datacite_data')
                     print(datacite_data.keys())
@@ -56,24 +55,25 @@ Search for the URI using the resource title in search engines (Google)"""
         # print(req.json())
 
 
-        # Check google search using the resource title and its alternative URIs
-        if 'resource_title' in eval.data.keys():
-            resource_title = eval.data['resource_title']
+        ## Check google search using the resource title and its alternative URIs
+        ## Might be against Google TOS
+        # if 'resource_title' in eval.data.keys():
+        #     resource_title = eval.data['resource_title']
             
-            resource_uris = eval.data['alternative_uris']
+        #     resource_uris = eval.data['alternative_uris']
 
-            self.check('Running Google search for: ' + resource_title)
-            search_results = list(search(resource_title, tld="co.in", num=20, stop=20, pause=1))
-            print(search_results)
+        #     self.check('Running Google search for: ' + resource_title)
+        #     search_results = list(search(resource_title, tld="co.in", num=20, stop=20, pause=1))
+        #     print(search_results)
 
-            found_uris = list(set(resource_uris).intersection(search_results))
-            # if any(i in resource_uris for i in search_results):
-            if found_uris:
-                self.success('Found the resource URI ' + ', '.join(found_uris) + ' when searching on Google for ' + resource_title)
-            else:
-                self.error('Did not find one of the resource URIs ' + ', '.join(resource_uris) + ' in: '+ ', '.join(search_results))
-        else:
-            self.error('No resource title found, cannot search in google')
+        #     found_uris = list(set(resource_uris).intersection(search_results))
+        #     # if any(i in resource_uris for i in search_results):
+        #     if found_uris:
+        #         self.success('Found the resource URI ' + ', '.join(found_uris) + ' when searching on Google for ' + resource_title)
+        #     else:
+        #         self.error('Did not find one of the resource URIs ' + ', '.join(resource_uris) + ' in: '+ ', '.join(search_results))
+        # else:
+        #     self.error('No resource title found, cannot search in google')
 
             
         return eval, g 
