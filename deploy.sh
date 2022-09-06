e8e8a2ece93554a080fb3b84d7271fb5078f00c3
Vincent Emonet <vincent.emonet@gmail.com>
2022-09-06 12:57:12 +0200

    try to fix poetry going full retard

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@
 #ssh ids3 'cd /data/deploy-services/fair-enough ; git pull ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --force-recreate --build --remove-orphans -d'
 
 ## Build without cache:
-ssh ids3 'cd /data/deploy-services/fair-enough ; git pull ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml down ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml build --no-cache ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d'
+ssh ids3 'cd /data/deploy-services/fair-enough ; git pull ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml down ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml build --no-cache ; docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --force-recreate'
