41a94703cfb35bd286e977fd4c8e5ff942ed7894
Vincent Emonet <vincent.emonet@gmail.com>
2022-09-06 12:22:44 +0200

    try to fix poetry going full retard

--- backend/prestart.sh
+++ backend/prestart.sh
@@ -4,4 +4,5 @@
 # python /app/app/backend_pre_start.py
 
 # Create initial data in DB
-poetry run python /app/app/initial_data.py
+python /app/app/initial_data.py
+# poetry run python /app/app/initial_data.py
